3509a0ab5e2789a39a92cdc5726e9bba842a84a9
joecryptotoo <80373433+joecryptotoo@users.noreply.github.com>
2023-08-10 09:58:25 -0700

    Update transport.rs

--- src/transport.rs
+++ src/transport.rs
@@ -46,7 +46,8 @@ impl From<rppal::gpio::Error> for Error {
         // Here, you'll need to map or convert the `rppal::gpio::Error` into an appropriate variant or representation of your custom `Error` type
         // This will depend on how your custom `Error` type is structured
         // As an example, you might have something like this:
-        Error::SerialPort(err)
+        //Error::SerialPort(err)
+        Error::timeout()
     }
 }
 
