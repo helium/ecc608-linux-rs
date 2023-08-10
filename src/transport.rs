5c63e994e63b9a4d04f898570375ea37baff0a2c
joecryptotoo <80373433+joecryptotoo@users.noreply.github.com>
2023-08-09 22:10:15 -0700

    Update transport.rs

--- src/transport.rs
+++ src/transport.rs
@@ -86,12 +86,6 @@ impl I2cTransport {
     }
 
     fn send_wake(&mut self, wake_delay: Duration) -> Result {
-        let _ = self.send_buf(0, &[0x00]);
-        thread::sleep(wake_delay);
-        Ok(())
-    }
-
-    fn send_wake(&mut self, wake_delay: Duration) -> Result {
         // Number of times you want to send 0x00
         let num_zeros_to_send = 3; // You can change this to the desired number
         let zeros = vec![0x00; num_zeros_to_send];
