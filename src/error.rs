18231d87801c89dddb79e9216edad43a51e7ef47
Farukest <abdullahfarukozden@gmail.com>
2024-03-25 09:20:55 +0300

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,22 @@
+use std::io::Error as IoError;
+use serialport::Error as SerialPortError;
+use ecc608_linux::error::Error as Ecc608LinuxError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("io error")]
-    IoError(#[from] std::io::Error),
+    IoError(#[from] IoError),
     #[error("timeout/retry error")]
     Timeout,
     #[error("ecc error {:?}", .0)]
     Ecc(crate::command::EccError),
     #[error("serial port error")]
-    SerialPort(#[from] serialport::Error),
+    SerialPort(#[from] SerialPortError),
     #[error("invalid ecc address")]
     InvalidAddress,
+    #[error("ecc608 linux error {:?}", .0)]
+    Ecc608LinuxError(#[from] Ecc608LinuxError),
 }
 
 impl Error {
